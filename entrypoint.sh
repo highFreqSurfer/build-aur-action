904e2b56c9fb77c3a839996041fed52b98ef4171
住所不定無職低収入 <pan1cAttack@icloud.com>
2025-01-14 17:28:34 +0800

    fork

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+set -x
+
 git clone "https://aur.archlinux.org/$1.git"
 cd "$1"
 makepkg -sf --noconfirm
+
+if [ $2 = "true" ]; then
+  echo building repo db ...
+  repo-add repo.db.tar.zst *.tar.zst
+  rm *.db *.files
+  mv repo.db.tar.zst repo.db
+  mv repo.files.tar.zst repo.files
+  chmod o+rw repo.db repo.files
+fi 
+
+chmod o+rw *.tar.zst
